2354caa59b8df64be80d595b25f1e722c4493c35
gokulanand03@gmail.com <gokulanand03@gmail.com>
2020-05-03 20:42:00 +0530

    commit

---
+++ Kerala_centroid.js
@@ -0,0 +1,22 @@
+var points = [{
+"type": "FeatureCollection",
+"name": "Kerala_centroid",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "DISTRICT": "Alappuzha", "Density": 0 }, "geometry": { "type": "Point", "coordinates": [ 76.449579739386508, 9.423557967696709 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Ernakulam", "Density": 0 }, "geometry": { "type": "Point", "coordinates": [ 76.472681712267416, 10.057657883287545 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Idukki", "Density": 14 }, "geometry": { "type": "Point", "coordinates": [ 77.027463327843876, 9.88219788260157 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Kannur", "Density": 43 }, "geometry": { "type": "Point", "coordinates": [ 75.526237737426484, 12.000633120778058 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Kasaragod", "Density": 5 }, "geometry": { "type": "Point", "coordinates": [ 75.153681569352827, 12.462817505328784 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Kollam", "Density": 12 }, "geometry": { "type": "Point", "coordinates": [ 76.874869817365422, 8.96099052098878 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Kottayam", "Density": 18 }, "geometry": { "type": "Point", "coordinates": [ 76.650919350832709, 9.637065459866321 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Kozhikode", "Density": 4 }, "geometry": { "type": "Point", "coordinates": [ 75.834361239127617, 11.484801054131948 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Malappuram", "Density": 2 }, "geometry": { "type": "Point", "coordinates": [ 76.155781148300079, 11.132066105784393 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Palakkad", "Density": 1 }, "geometry": { "type": "Point", "coordinates": [ 76.558583645299734, 10.79464117363883 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Pathanamthitta", "Density": 1 }, "geometry": { "type": "Point", "coordinates": [ 76.9289672359441, 9.284060659374349 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Thiruvananthapuram", "Density": 2 }, "geometry": { "type": "Point", "coordinates": [ 77.015000319667664, 8.60845769871491 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Thrissur", "Density": 0 }, "geometry": { "type": "Point", "coordinates": [ 76.31615029357242, 10.471177987969559 ] } },
+{ "type": "Feature", "properties": { "DISTRICT": "Wayanad", "Density": 0 }, "geometry": { "type": "Point", "coordinates": [ 76.098133261934791, 11.711246387121113 ] } }
+]
+}
+]
